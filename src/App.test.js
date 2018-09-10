a3a47f64280a171f6ebebf4c2be6e8a99519d912
juliamazur <julasia@gmail.com>
2018-09-10 11:13:12 +0200

    1 add basic nav bar

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Header from './components/Header';
+import {
+    shallow
+} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    const div = document.createElement('div');
+    ReactDOM.render( < App / > , div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+it('Header is selectable by class "App-header"', function() {
+    expect(shallow( < Header / > ).is('.App-header')).toBe(true);
+});
+
+it('Header title mounts in a full DOM', function() {
+    expect(mount(< Header />).find('.App-title').length).toBe(1);
 });
